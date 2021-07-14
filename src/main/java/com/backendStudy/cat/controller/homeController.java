244f5754750f4a728179b90255a3732e2eda88fc
Da-bin-kim <omming@naver.com>
2021-07-14 20:05:56 +0900

    회원가입 페이지 추가

--- src/main/java/com/backendStudy/cat/controller/homeController.java
+++ src/main/java/com/backendStudy/cat/controller/homeController.java
@@ -10,4 +10,9 @@ public class homeController {
     public String login(){
         return "login";
     }
+
+    @GetMapping("/signup")
+    public String signup(){
+        return "signup";
+    }
 }
