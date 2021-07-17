3ed976e54d640595a306455e22a3e547ab1fe558
KNH <nh2004@naver.com>
2021-07-17 18:48:20 +0900

    메인화면 fragment 적용 및 controller 추가
    home.html에 fragment 적용
    controller 추가

---
+++ src/main/java/com/backendStudy/cat/controller/rootController.java
@@ -0,0 +1,12 @@
+package com.backendStudy.cat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class rootController {
+    @GetMapping("/")
+    public String mainForm(){
+        return "home";
+    }
+}
