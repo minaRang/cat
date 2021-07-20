0416ff3dd84b04dbf4a40db1893a10ca83cc31c2
KNH <nh2004@naver.com>
2021-07-20 19:14:44 +0900

    HomeController, WriteController 작성
    HomeController 와 WriteController 작성
    
    HomeController
    boardList를 탭에 따라 적절히 정렬하여 home.html에 보여줌
    
    WriteController
    게시글 작성 페이지에서 작성한 폼을 DB로 전송
    (파일 전송은 불가능)

--- src/main/java/com/backendStudy/cat/controller/WriteController.java
+++ src/main/java/com/backendStudy/cat/controller/WriteController.java
@@ -1,20 +1,41 @@
 package com.backendStudy.cat.controller;
 
+import com.backendStudy.cat.domain.DTOBoard;
+import com.backendStudy.cat.mapper.BoardMapper;
+import com.backendStudy.cat.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WriteController {
-    @GetMapping("/new")
-    public String mainForm(){
-        return "new";
+    @Autowired
+    BoardService boardService;
+    @GetMapping("/write")
+    public String MainForm(Model model){
+        model.addAttribute("board",new DTOBoard());
+        return "board/write";
     }
 
-    @PostMapping("/questions")
-    public String newQuestion(BoardForm form){
+    @PostMapping("/form")
+    public String NewQuestion(@RequestParam String boardTitle,
+                              @RequestParam String boardContent,
+                              Model model){
+        DTOBoard board = new DTOBoard();
+        board.setBoardTitle(boardTitle);
+        board.setBoardContent(boardContent);
 
+        //test용
+        //TODO : 로그인한 계정의 idx 값 저장으로 수정
+        board.setUserIdx(Long.valueOf(2));
 
+        //TODO: tag와 file 구현
+        //TODO: 게시글 작성시 작성자를 편집 히스토리에 추가
+
+        boardService.registerBoard(board);
         return "redirect:/";
     }
 }
