a053f348e05470581e03ae65b0d7ac1dc36dc91e
KNH <nh2004@naver.com>
2021-07-24 01:47:13 +0900

    HomeController 및 home.html 수정
    페이징 처리를 위해 HomeController 및 home.html 수정

--- src/main/java/com/backendStudy/cat/controller/HomeController.java
+++ src/main/java/com/backendStudy/cat/controller/HomeController.java
@@ -2,64 +2,48 @@ package com.backendStudy.cat.controller;
 
 import com.backendStudy.cat.domain.DTOBoard;
 import com.backendStudy.cat.service.BoardService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-
+@Slf4j
 @Controller
 public class HomeController {
 
     @Autowired
     private BoardService boardService;
+
+
     @GetMapping("/")
-    public String Home(Model model){
-        List<DTOBoard> boardList= boardService.getBoardList();
-        boardList.sort((a,b)->b.getDate().compareTo(a.getDate()));
-        boardList.forEach(b->b.setTimeInterval(CalDate(b.getDate())));
+    public String Home(@ModelAttribute("board")DTOBoard board,
+                       Model model){
+        List<DTOBoard> boardList= boardService.getBoardListOrderByDate(board);
         model.addAttribute("boardList",boardList);
         model.addAttribute("tap","home");
         return "board/home";
     }
     @GetMapping("/popular")
-    public String PopularList(Model model){
-        List<DTOBoard> boardList= boardService.getBoardList();
-        Collections.sort(boardList,(list1, list2)-> {
-            return Long.valueOf(list2.getBoardView() - list1.getBoardView()).intValue();
-        });
-        boardList.forEach(b->b.setTimeInterval(CalDate(b.getDate())));
+    public String PopularList(@ModelAttribute("board")DTOBoard board,
+                              Model model){
+        List<DTOBoard> boardList= boardService.getBoardListOrderByPopular(board);
         model.addAttribute("boardList",boardList);
         model.addAttribute("tap","popular");
         return "board/home";
     }
     @GetMapping("/needAnswer")
-    public String NeedAnswerList(Model model){
-        List<DTOBoard> boardList= boardService.getBoardList();
-        boardList.removeIf(m->m.getCntBoardAnswer()>0);
-        boardList.sort((a,b)->b.getDate().compareTo(a.getDate()));
-        boardList.forEach(b->b.setTimeInterval(CalDate(b.getDate())));
+    public String NeedAnswerList(@ModelAttribute("board")DTOBoard board,
+                                 Model model){
+        List<DTOBoard> boardList= boardService.getBoardListOrderByNeedAnswer(board);
         model.addAttribute("boardList",boardList);
         model.addAttribute("tap","needAnswer");
         return "board/home";
     }
 
-    //날짜 계산 함수
-    public String CalDate(Date date) {
-        Date now = new Date();
-        Long calDate = now.getTime() - date.getTime();
-
-        Long calTime = calDate / (60 * 1000);
-
-        if (calTime <= 60) return calTime + "분 전";
-        else if (calTime <= 60*24) return calTime/60+ "시간 전";
-        else if(calTime<=60*24*30) return calTime/(60*24)+"일 전";
-        else if (calTime<=60*24*30*12) return calTime/(60*24*30) +"달 전";
-        else return calTime/(60*24*30*12) +"년 전";
-    }
 
 }
