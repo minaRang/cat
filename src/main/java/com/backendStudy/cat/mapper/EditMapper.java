ea272eb48f2b440b85e3bf7ae44545f0cc963d6d
yoohyunju <yhj2756@naver.com>
2021-07-27 23:54:15 +0900

    mapper 어노테이션 추가
    mapper 어노테이션 추가

--- src/main/java/com/backendStudy/cat/mapper/EditMapper.java
+++ src/main/java/com/backendStudy/cat/mapper/EditMapper.java
@@ -1,7 +1,9 @@
 package com.backendStudy.cat.mapper;
 
 import com.backendStudy.cat.domain.DTOEdit;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface EditMapper {
 
     public Long insertEditContent(DTOEdit edit);
