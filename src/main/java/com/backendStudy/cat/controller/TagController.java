6dfdff0cdbd70d0722c615a583a7f682e3aa1793
KNH <nh2004@naver.com>
2021-07-28 23:16:52 +0900

    write 컨트롤러 수정 및 view, tag 컨트롤러 수정
    write 컨트롤러
    - 게시글 작성시 태그 추가
    
    view 컨트롤러
    게시글의 상세 내용 표시 및 조회수 기능 추가
    
    tag 컨트롤러
    자동 완성 기능 구현

---
+++ src/main/java/com/backendStudy/cat/controller/TagController.java
@@ -0,0 +1,25 @@
+package com.backendStudy.cat.controller;
+
+import com.backendStudy.cat.domain.DTOTag;
+import com.backendStudy.cat.mapper.TagMapper;
+import com.backendStudy.cat.service.TagService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("searchTag")
+public class TagController {
+    @Autowired
+    TagService tagService;
+
+    @GetMapping
+    public List<DTOTag> autoTagName(@RequestParam String tagName){
+        List<DTOTag> dtoTags=tagService.autoMatchingTag(tagName);
+        return tagService.autoMatchingTag(tagName);
+    }
+}
