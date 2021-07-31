1958c627b14b4b6d52768262a7555d6598fe3995
yoohyunju <yhj2756@naver.com>
2021-08-01 00:06:35 +0900

    EditMapper 및 DTOEdit 수정
    EditMapper 및 DTOEdit 수정

--- src/main/java/com/backendStudy/cat/domain/DTOEdit.java
+++ src/main/java/com/backendStudy/cat/domain/DTOEdit.java
@@ -3,14 +3,23 @@ package com.backendStudy.cat.domain;
 import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class DTOEdit {
+
     private long editIdx;        // 인덱스
     private long boardIdx;       // 게시글인덱스
+    private long answerIdx;      // 답변인덱스
     private long userIdx;        // 유저인덱스
     private String editTitle;       // 제목
     private String editContent;     // 내용
     private Date date;     // 등록일
+    private Date updateTime; //수정일
+    private long editExist; //편집 요청 존재 여부
+    private long editApproval; //편집 요청 승인 여부
+    private String editMessage; //편집 요청 메시지
+    private String editTagName; //태그 편집
 }
--- src/main/java/com/backendStudy/cat/mapper/EditMapper.java
+++ src/main/java/com/backendStudy/cat/mapper/EditMapper.java
@@ -3,9 +3,16 @@ package com.backendStudy.cat.mapper;
 import com.backendStudy.cat.domain.DTOEdit;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface EditMapper {
 
-    public Long insertEditContent(DTOEdit edit);
-    public Long selectCountEdit();
+    public int insertEditContent(DTOEdit edit); //편집요청 내용 등록
+    public Long selectCountEdit(); //편집요청 개수
+    public List<DTOEdit> selectEditList(); //편집요청 리스트
+    public int updateEdit(DTOEdit edit); //편집요청 수정
+    public int deleteEdit(Long index);//편집 요청 삭제
+    public int approvalEdit(Long index); //편집 요청 승인
+    public int rejectEdit(Long index); //편집 요청 거절
 }
