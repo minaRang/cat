06da22a09064da8ddfc38d12305d5296467257af
KNH <nh2004@naver.com>
2021-08-08 01:41:36 +0900

    springboot timezone 설정
    springboot timezone 설정

--- src/main/java/com/backendStudy/cat/CatApplication.java
+++ src/main/java/com/backendStudy/cat/CatApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CatApplication {
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CatApplication.class, args);
 	}
