43768ac01cdc1e7f4cedcee9f68be3a08a39d7b1
KNH <nh2004@naver.com>
2021-08-08 01:29:08 +0900

    운영 환경 db 변경 및 QNAController 주석처리
    운영 환경에서 rds를 사용하도록 다시 변경
    questions를 매핑하는 controller 가 2개여서 QNAController를 주석처리 해두었습니다.

--- src/main/java/com/backendStudy/cat/controller/QnaController.java
+++ src/main/java/com/backendStudy/cat/controller/QnaController.java
@@ -3,12 +3,12 @@ package com.backendStudy.cat.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class QnaController {
-
-    @GetMapping("/questions")
-    public String login(){
-        return "questions";
-    }
-
-}
+//@Controller
+//public class QnaController {
+//
+//    @GetMapping("/questions")
+//    public String login(){
+//        return "questions";
+//    }
+//
+//}
